d4bd9684d167680ac216e73969af7a01d40e1d6f
Stuart Larsen <sclarsen@mtu.edu>
2014-08-20 21:51:16 -0400

    Added filter type

---
+++ models/filter.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var FilterSchema = new Schema({
+	exclude: {type: Boolean, default: false },
+	field: { type: String, default: "blocked-uri" },
+	expression: {type: String, default: "/^https/" },
+	name: { type: String, default: "HTTPS" },
+
+	filter: {type: Schema.Types.ObjectId, ref: "Project" }
+});
+
+module.exports = mongoose.model('Filter', FilterSchema);
